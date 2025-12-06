d42899bd8029428b5797bef7a5ee484e3406f22a
Khadija Gwarzo <khadijagarbag@gmail.com>
2025-12-06 23:16:14 +0100

    chore(newsletter): fix image bug

--- src/app/api/contentful-webhook/route.ts
+++ src/app/api/contentful-webhook/route.ts
@@ -15,7 +15,7 @@ export async function POST(request: Request) {
       body.fields?.shortDescription?.["en-US"] ||
       "A new smart home innovation has been published";
     const availability = body.fields?.availability?.["en-US"] || "";
-    const imageId = body.fields?.featuredImage?.fields.file.url["en-US"] || "";
+    const imageId = body.fields?.featuredImage?.["en-US"]?.sys.id || "";
     const featuredImage = imageId ? `${process.env.LOGO_URL!}/${imageId}` : "";
     const entryUrl = `${process.env.SITE_URL!}/smart-housing/${slug}`;
     const logo = `${process.env.LOGO_URL!}/6pg8lZdUUlStkfYKa1mEHe/f5c7dbf09c064805872b773bed9e3705/Frame_63.png`;
