cc29333690738f8a4d5fb28250e4c0652cf5aaa3
Khadija Gwarzo <khadijagarbag@gmail.com>
2025-12-06 23:06:58 +0100

    chore(newsletter): change integration

--- src/app/api/contentful-webhook/route.ts
+++ src/app/api/contentful-webhook/route.ts
@@ -16,7 +16,7 @@ export async function POST(request: Request) {
       "A new smart home innovation has been published";
     const availability = body.fields?.availability?.["en-US"] || "";
     const featuredImage =
-      body.fields?.featuredImage?.["en-US"].fields.file.url["en-US"] || "";
+      body.fields?.featuredImage?.fields.file.url["en-US"] || "";
     const entryUrl = `${process.env.SITE_URL!}/smart-housing/${slug}`;
     const logo = `${process.env.LOGO_URL!}/6pg8lZdUUlStkfYKa1mEHe/f5c7dbf09c064805872b773bed9e3705/Frame_63.png`;
 
